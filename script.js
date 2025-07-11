c7b0420a7748dbfeeb698e8105e235ac70b38b37
Shalama <shalamafadillaa@gmail.com>
2025-07-11 11:57:58 +0700

    Create a conditional statement
    Create the conditional statement for the randomNumber. If the number is 0, it will return 'Rock'. If it's 1 it will return 'Paper'. Or else it will return 'Scissors'.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let computerChoice = getComputerChoice();
 
-function getComputerChoice() {
+function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber == 0){
+        return "Rock";
+    }
+    else if (randomNumber == 1){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
 }
